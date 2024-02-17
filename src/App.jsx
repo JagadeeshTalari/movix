ee13b8081c83aeaf30e978b7fe76555f987e98f9
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2024-02-17 22:58:04 +0530

    Routing setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import { useEffect } from 'react'
-import './App.scss'
-import { fetchDataFromApi } from './utils/api'
-import { useDispatch, useSelector } from 'react-redux'
-import { getApiConfig } from './features/home/homeSlice'
-import Home from './pages/home/Home'
+import { useEffect } from "react";
+import "./App.scss";
+import { fetchDataFromApi } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfig } from "./features/home/homeSlice";
+import { Details, Explore, Home, PageNotFound, SearchResult } from "./pages";
+import { Header, Footer } from "./components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const {url} = useSelector(state => state.home)
-  const dispatch = useDispatch()
+  const { url } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
   useEffect(() => {
-   
-      fetchDataFromApi("/movie/popular").then(res => {console.log(res)
-      dispatch(getApiConfig(res))}
-      )
-      
-  },[])
-  return(
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfig(res));
+    });
+  }, []);
+  return (
     <div>
-      {
-        url?.total_pages
-      }
-      <Home/>
+      {url?.total_pages}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/serach/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
